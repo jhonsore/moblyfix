c3efcb6801bfb938a9bb44c05f5313ddcfd5df35
gilesonalves <gilesonalves@gmail.com>
2025-02-11 17:56:48 -0300

    pagina store responsivo pronto

--- src/components/headerPage/index.tsx
+++ src/components/headerPage/index.tsx
@@ -5,7 +5,7 @@ const HeaderPage = ({ title, children }: { title: string, children?: ReactNode }
     const { open } = useSidebar()
 
     return <div className="flex  items-center pr-4 bg-white  h-[60px] border-b border-gray-200 sm:pr-6 fixed top-0 left-0 w-full   z-20 ">
-        <div className={`flex duration-200 ease-linear items-center justify-between pl-6 flex-wrap sm:flex-nowrap ${open ? 'md:ml-[--sidebar-width]' : 'ml-[48px]'}   duration-200 w-full  `}>
+        <div className={`flex duration-200 ease-linear items-center justify-between pl-2 lg:pl-6 flex-wrap sm:flex-nowrap ${open ? 'md:ml-[--sidebar-width]' : 'ml-[48px]'}   duration-200 w-full  `}>
             <div className=" flex items-center">
                 <SidebarTrigger />
                 <h3 className="text-lg leading-6 font-bold text-black">{title}</h3>
--- src/screens/stores/index.tsx
+++ src/screens/stores/index.tsx
@@ -57,9 +57,9 @@ const PageStores = () => {
         </ HeaderPage>
         <PageContent>
 
-            <div className=" py-6 hidden lg:block">
+            <div className=" py-6 ">
                 <table className=" w-full">
-                    <thead className="bg-gray-50">
+                    <thead className="bg-gray-50 hidden lg:table-header-group w-full">
                         <tr>
                             <th
                                 scope="col"
@@ -73,25 +73,45 @@ const PageStores = () => {
                             >
                                 Whatsapp
                             </th>
-
                         </tr>
                     </thead>
                     <tbody className=" bg-white">
                         {pageData.map((data) => (
-                            <tr key={data._id} className='border-b border-gray-200'>
-                                <td className="whitespace-nowrap py-4 text-sm text-gray-900 ">
-                                    {data.name}
+                            <tr key={data._id} className='lg:border-b border-gray-200 '>
+                                <td className="whitespace-nowrap lg:px-2 lg:py-4 text-sm text-gray-900 block lg:table-cell p-0">
+                                    <div className="flex justify-between">
+                                        <div className="lg:hidden w-3/5 bg-gray-50 p-4 lg:p-0  text-left text-sm font-semibold text-gray-900 ">
+                                            Nome
+                                        </div>
+                                        <div className="text-sm text-gray-900 p-4 lg:p-0">
+                                            {data.name}
+                                        </div>
+                                    </div>
                                 </td>
-                                <td className="whitespace-nowrap px-2 py-3 text-sm text-gray-900">{data.whatsapp}</td>
-                                <td className="relative whitespace-nowrap py-2 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                                <td className="whitespace-nowrap text-sm text-gray-900 block lg:table-cell p-0">
+                                    <div className="flex justify-between">
+                                        <div className="lg:hidden w-3/5 bg-gray-50 p-4 lg:p-0  text-left text-sm font-semibold text-gray-900">
+                                            Whatsapp
+                                        </div>
+                                        <div className="text-sm text-gray-900 p-4">
+                                            {data.name}
+                                        </div>
+                                    </div>
+                                </td>
+                                <td className=" whitespace-nowrap pl-3 text-center lg:text-right text-sm font-medium sm:pr-6">
                                     <Link to={'/dashboard/lojas/1'}>
-                                        <span className="text-gray-400 hover:text-indigo-900 mr-2">
+                                        <span className="text-gray-400 hover:text-indigo-900 mr-2 hidden lg:inline">
                                             <span className="material-symbols-outlined">
                                                 edit
                                             </span>
                                         </span>
+                                        <span className="text-gray-400 hover:text-indigo-900 lg:mr-2 lg:hidden flex justify-center ">
+
+                                            <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+
+                                        </span>
                                     </Link>
-                                    <a href="#" className="text-gray-400 hover:text-indigo-900">
+                                    <a href="#" className="text-gray-400 hover:text-indigo-900 hidden lg:inline">
                                         <span className="material-symbols-outlined">
                                             delete
                                         </span><span className="sr-only">, {data._id}</span>
@@ -99,47 +119,11 @@ const PageStores = () => {
                                 </td>
                             </tr>
                         ))}
+
                     </tbody>
                 </table>
             </div>
 
-            {/* <div className=" bg-white m-w-full py-6 lg:hidden">
-                <table className=" w-full">
-
-                    {pageData.map((transaction) => (
-                        <tr key={transaction.id} className='border-y border-gray-200'>
-                            <td className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900 bg-gray-50">
-                                Nome
-                            </td>
-                            <td className="whitespace-nowrap py-4 pl-3 text-right pr-3 text-sm text-gray-900 border-gray-200">
-                                {transaction.id}
-                            </td>
-
-                            <td rowSpan={2} className="text-center border">
-                                <Link to={'/dashboard/ordem-servico/analise-tecnica'}>
-                                    <span className="material-symbols-outlined text-gray-400 hover:text-indigo-900 mr-2">
-                                        visibility
-                                    </span>
-                                </Link>
-                            </td>
-                        </tr>
-                    ))}
-                    {transactions.map((transaction) => (
-                        <tr key={transaction.id} className='border-b-4 border-gray-200'>
-                            <td className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900 bg-gray-50">
-                                Whatsapp
-                            </td>
-                            <td className="whitespace-nowrap py-4 pl-3 text-right pr-3 text-sm text-gray-900">
-                                {transaction.commission}
-                            </td>
-
-                        </tr>
-                    ))}
-
-
-
-                </table>
-            </div> */}
             <div className='py-4 flex justify-end'>
                 <button
                     type="button"
