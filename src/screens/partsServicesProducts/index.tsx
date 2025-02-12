aec4e4ceb9b223d66b34b1aeb3eadfd13251cd95
gilesonalves <gilesonalves@gmail.com>
2025-02-12 18:41:56 -0300

    ajustes no responsivo, versao com uma tabela so

--- src/screens/partsServicesProducts/index.tsx
+++ src/screens/partsServicesProducts/index.tsx
@@ -12,10 +12,6 @@ import { ErrorPage } from "@/components/errorPage"
 import { TypePartsServicesProducts } from "@/types/PartsServicesProducts"
 
 
-
-
-
-
 const PagePartsServicesProducts = () => {
   const { db } = useFirebaseContext()
   const [pageData, setPageData] = useState<TypePartsServicesProducts[]>([])
@@ -56,13 +52,13 @@ const PagePartsServicesProducts = () => {
     <PageContent>
 
 
-      <div className="py-6 hidden lg:block">
+      <div className="py-6">
         <table className=" w-full">
-          <thead className="bg-gray-50">
+          <thead className="bg-gray-50 hidden lg:table-header-group w-full">
             <tr>
               <th
                 scope="col"
-                className="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
               >
                 Nome
               </th>
@@ -74,42 +70,73 @@ const PagePartsServicesProducts = () => {
               </th>
               <th
                 scope="col"
-                className="whitespace-nowrap px-2 py-4 text-left text-sm font-semibold text-gray-900"
+                className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
               >
                 Venda à vista
               </th>
               <th
-                scope="col"
+                colSpan={2}
                 className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
               >
                 Venda à prazo
-              </th>
-              <th>
               </th>
             </tr>
           </thead>
           <tbody className=" bg-white">
             {pageData.map((data) => (
-              <tr key={data._id} className='border-b border-gray-200'>
-                <td className="whitespace-nowrap py-4 pl-3 pr-3 text-sm text-gray-900 sm:pl-6">
-                  {data.name}
+              <tr key={data._id} className='lg:border-b border-gray-200'>
+                <td className="whitespace-nowrap lg:px-2 lg:py-4 text-sm text-gray-900 block lg:table-cell p-0 border-b border-gray-200">
+                  <div className="flex justify-between">
+                    <div className="lg:hidden w-3/5 bg-gray-50 p-4 lg:p-0  text-left text-sm font-semibold text-gray-900 ">
+                      Nome
+                    </div>
+                    <div className="text-sm text-gray-900 p-4 lg:p-0">
+                      {data.name}
+                    </div>
+                  </div>
                 </td>
-                <td className="whitespace-nowrap px-2 py-3 text-sm font-medium text-gray-900">
-                  {data.costPrice}
+                <td className="whitespace-nowrap lg:px-2 lg:py-4 text-sm text-gray-900 block lg:table-cell p-0 border-b border-gray-200">
+                  <div className="flex justify-between">
+                    <div className="lg:hidden w-3/5 bg-gray-50 p-4 lg:p-0  text-left text-sm font-semibold text-gray-900 ">
+                      Preço de custo
+                    </div>
+                    <div className="text-sm text-gray-900 p-4 lg:p-0">
+                      {data.costPrice}
+                    </div>
+                  </div>
                 </td>
-                <td className="whitespace-nowrap px-2 py-3 text-sm text-gray-900">{data.cashPrice}</td>
-                <td className="whitespace-nowrap px-2 py-3 text-sm text-gray-900">{data.installmentPrice}</td>
-                <td className="relative whitespace-nowrap py-2 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                <td className="whitespace-nowrap lg:px-2 lg:py-4 text-sm text-gray-900 block lg:table-cell p-0 border-b border-gray-200">
+                  <div className="flex justify-between">
+                    <div className="lg:hidden w-3/5 bg-gray-50 p-4 lg:p-0  text-left text-sm font-semibold text-gray-900 ">
+                      Venda à vista
+                    </div>
+                    <div className="text-sm text-gray-900 p-4 lg:p-0">
+                      {data.cashPrice}
+                    </div>
+                  </div>
+                </td>
+                <td className="whitespace-nowrap lg:px-2 lg:py-4 text-sm text-gray-900 block lg:table-cell p-0 border-b-4 border-gray-200 lg:border-none">
+                  <div className="flex justify-between">
+                    <div className="lg:hidden w-3/5 bg-gray-50 p-4 lg:p-0  text-left text-sm font-semibold text-gray-900 ">
+                      Venda à prazo
+                    </div>
+                    <div className="text-sm text-gray-900 p-4 lg:p-0">
+                      {data.installmentPrice}
+                    </div>
+                  </div>
+                </td>
+                <td className="whitespace-nowrap pl-3 text-center lg:text-right text-sm font-medium sm:pr-6 ">
                   <Link to={'/dashboard/pecas-servicos/1'}>
-                    <span className="text-gray-400 hover:text-indigo-900 mr-2">
-                      <span className="material-symbols-outlined">
-                        edit
-                      </span>
+                    <span className="material-symbols-outlined text-gray-400 hover:text-indigo-900 mr-2 hidden lg:inline">
+                      edit
+                    </span>
+                    <span className="text-gray-400 hover:text-indigo-900 lg:hidden flex justify-center ">
+                      <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
                     </span>
                   </Link>
 
                   <a href="#" className="text-gray-400 hover:text-indigo-900">
-                    <span className="material-symbols-outlined">
+                    <span className="material-symbols-outlined text-gray-400 hover:text-indigo-900 mr-2 hidden lg:inline">
                       delete
                     </span><span className="sr-only">, {data._id}</span>
                   </a>
@@ -122,7 +149,7 @@ const PagePartsServicesProducts = () => {
 
 
 
-      <div className=" bg-white m-w-full py-6 lg:hidden">
+      {/* <div className=" bg-white m-w-full py-6 lg:hidden">
 
 
 
@@ -139,7 +166,7 @@ const PagePartsServicesProducts = () => {
                     {data.name}
                   </div>
                 </div>
-<div>
+                <div>
                   <div className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900 bg-gray-50">
                     Nome
                   </div>
@@ -164,7 +191,7 @@ const PagePartsServicesProducts = () => {
 
 
         </div>
-      </div>
+      </div> */}
 
       <div className='py-4 flex justify-end'>
         <button
