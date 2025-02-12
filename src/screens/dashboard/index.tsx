ded0db89cd8f453c0fced2ce1e3cc3d52f6ddca0
gilesonalves <gilesonalves@gmail.com>
2025-02-12 08:35:29 -0300

    pagina peças/serviços

--- src/screens/dashboard/index.tsx
+++ src/screens/dashboard/index.tsx
@@ -1,12 +1,16 @@
-
 import HeaderPage from "../../components/headerPage"
 import PageContent from "../../components/layout/pageContent"
+import { Button } from "../../components/ui/button"
+
 
 const PageDashboard = () => {
 
     return <>
         <HeaderPage title="Dashboard" />
         <PageContent>
+            <HeaderPage title="Finalizar venda">
+                <Button variant={"primary"}>Salvar</Button>
+            </ HeaderPage>
             aqui entra o dashboard
             <br /><br /><br />
         </PageContent>
