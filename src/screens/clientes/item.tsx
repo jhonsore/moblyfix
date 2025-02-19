6e734593c6776c61077363153f8fcbfae650b5f9
gilesonalves <gilesonalves@gmail.com>
2025-02-19 13:18:40 -0300

    formulario clientes

--- src/screens/clientes/item.tsx
+++ src/screens/clientes/item.tsx
@@ -13,13 +13,175 @@ import { Button } from "../../components/ui/button"
 import HeaderPage from "@/components/headerPage"
 import { Textarea } from "@/components/ui/textarea"
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
-import { Link } from "react-router"
+import { Link, useNavigate } from "react-router"
 import { useParams } from "react-router"
+import { useFirebaseContext } from "@/providers/firebase/useFirebaseContext"
+import { useStoresContext } from "@/providers/stores/useStoresContext"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useEffect, useState } from "react"
+import { TypePageStatus } from "@/types/PageStatus"
+import { DB } from "@/functions/database"
+import { LoadingPage } from "@/components/loadingPage"
+import { ErrorPage } from "@/components/errorPage"
 
+
+const FormSchema = z.object({
+    name: z
+        .string().min(1, {
+            message: "Preencha o nome do usuário",
+        }),
+    cpfCnpj: z
+        .string().min(1, {
+            message: "Preencha o CPF do usuário",
+        }),
+    email: z
+        .string().min(1, {
+            message: "Preencha o Email",
+        }),
+    whatsapp: z
+        .string().min(1, {
+            message: "Preencha o whatsapp",
+        }),
+    phone: z
+        .string(),
+    phone2: z
+        .string(),
+    phone3: z
+        .string(),
+    city: z
+        .string().min(1, {
+            message: "Preencha a cidade",
+        }),
+    neighborhood: z
+        .string().min(1, {
+            message: "Preencha o bairro",
+        }),
+    address: z
+        .string().min(1, {
+            message: "Preencha o endereço",
+        }),
+    zipcode: z
+        .string().min(1, {
+            message: "Preencha o CEP",
+        }),
+    number: z
+        .string().min(1, {
+            message: "Preencha o número",
+        }),
+    state: z
+        .string().min(1, {
+            message: "Preencha o estado",
+        }),
+    complement: z
+        .string().min(1, {
+            message: "Preencha o complemento",
+        }),
+
+})
 
 const DadosDoCliente = () => {
-    const form = useForm()
+
+    const { db } = useFirebaseContext()
+    const { store } = useStoresContext()
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            name: "",
+            cpfCnpj: "",
+            email: "",
+            whatsapp: "",
+            phone: "",
+            phone2: "",
+            phone3: "",
+            state: "",
+            city: "",
+            neighborhood: "",
+            address: "",
+            zipcode: "",
+            number: "",
+            complement: "",
+        },
+    })
     const { id } = useParams()
+    const [statusStore, setStatusStore] = useState(false)
+    const [statusCreated, setStatusCreated] = useState(false)
+    const [pageStatus, setPageStatus] = useState<TypePageStatus>(id ? 'loading' : 'success')
+    const navigate = useNavigate()
+    const [statusLoading, setStatusLoading] = useState(false)
+
+    useEffect(() => {
+        if (!id) return
+        const load = async () => {
+            const result = await DB.customers.read({ db, id })
+            let status: typeof pageStatus = 'success'
+            if (!result.status) {
+                status = 'error'
+                return
+            }
+
+            setPageStatus(status)
+            const { doc } = result
+            if (doc) {
+                form.setValue('name', doc.name)
+                form.setValue('cpfCnpj', doc.cpfCnpj)
+                form.setValue('email', doc.email)
+                form.setValue('whatsapp', doc.whatsapp)
+                form.setValue('phone', doc.phone)
+                form.setValue('phone2', doc.phone2)
+                form.setValue('phone3', doc.phone3)
+                form.setValue('state', doc.state)
+                form.setValue('city', doc.city)
+                form.setValue('neighborhood', doc.neighborhood)
+                form.setValue('address', doc.address)
+                form.setValue('zipcode', doc.zipcode)
+                form.setValue('number', doc.number)
+                form.setValue('complement', doc.complement)
+            }
+        }
+        load()
+    }, [id])
+
+    async function onSubmit(values: z.infer<typeof FormSchema>) {
+        if (!store) {
+            setStatusStore(true)
+            return
+        }
+        setStatusLoading(true)
+        const { name, email, whatsapp, phone, phone2, phone3, state, city, neighborhood, address, zipcode, number, complement, cpfCnpj, } = values
+        const result = !id ?
+            await DB.customers.create({
+                db,
+                data: { _id: "", createdAt: Timestamp.now(), lastOsNumber: 0, name, email, whatsapp, phone, phone2, phone3, state, city, neighborhood, address, zipcode, number, complement, cpfCnpj, _headquarterId: store._headquarterId, _storeId: store._id }
+            }) :
+            await DB.customers.update({
+                db,
+                id,
+                data: { name, email, whatsapp, phone, phone2, phone3, state, city, neighborhood, address, zipcode, number, complement, cpfCnpj, }
+            })
+        if (result.status) {
+            setStatusCreated(true)
+        }
+        setStatusLoading(false)
+    }
+
+    const onCreateHandler = () => {
+        setStatusCreated(false)
+        form.reset()
+        if (id) navigate('/dashboard/clientes/novo')
+
+    }
+
+    if (pageStatus === 'loading') {
+        return <LoadingPage />
+    }
+
+    if (pageStatus === 'error') {
+        return <ErrorPage />
+    }
+
+
+
     return <>
         <PageContent>
 
